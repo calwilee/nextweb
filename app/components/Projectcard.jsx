17e6da58ce3a4bc491025d0bb841c3c2563ecdd3
calwilee <calwinlee@gmail.com>
2024-06-26 20:08:37 -0700

    moretextbugs

--- app/components/Projectcard.jsx
+++ app/components/Projectcard.jsx
@@ -4,7 +4,7 @@ const Projectcard = ({imgUrl, title, description, link }) => {
   return (
     <div>
         <a className='relative group' target='_blank' href= {link}>
-            <div className='overlay absolute top-0 rounded-xl left-0 w-full h-full bg-[#6aa5e9] bg-opacity-0 hidden group-hover:flex  group-hover:bg-opacity-80 transition-all duration-300'> 
+            <div className='overlay absolute top-0 rounded-xl left-0 w-full h-full bg-[#6aa5e9] bg-opacity-0 hidden group-hover:flex  group-hover:bg-opacity-80 transition-all duration-300 text-white'> 
                 <p className='m-8'>{description}</p>
             </div>
             <div className='h-48 md:h-60 rounded-t-xl' style = {{background: `url(${imgUrl})`, backgroundSize: "cover"}}> </div>
