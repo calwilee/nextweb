a4405a0169ff58778d61fed87c96ba692503f7df
calwilee <calwinlee@gmail.com>
2024-06-29 16:58:46 -0700

    project formatting

--- app/components/Projectcard.jsx
+++ app/components/Projectcard.jsx
@@ -5,7 +5,7 @@ const Projectcard = ({imgUrl, title, description, link }) => {
     <div>
         <a className='relative group' target='_blank' href= {link}>
             <div className='overlay absolute top-0 rounded-xl left-0 w-full h-full bg-[#6aa5e9] bg-opacity-0 hidden group-hover:flex  group-hover:bg-opacity-80 transition-all duration-300 text-white'> 
-                <p className='m-8'>{description}</p>
+                <p className='m-8 md:text-sm lg:text-base'>{description}</p>
             </div>
             <div className='h-48 md:h-60 rounded-t-xl' style = {{background: `url(${imgUrl})`, backgroundSize: "cover"}}> </div>
             <div className='text-white rounded-b-xl bg-[#6aa5e9] py-6 px-4'>
--- app/components/Projectsection.jsx
+++ app/components/Projectsection.jsx
@@ -5,8 +5,8 @@ import description from './description';
 const projectsData = [
     {
         id: 2,
-        title: "League Exploratory Data Analysis",
-        description: "EDA for Leauge of Legends match data. Utilized Python, Pandas, and Numpy to clean Data as well as preform analysis.",
+        title: "League EDA",
+        description: "Exploratory data analysis for Leauge of Legends match data. Utilized Python, Pandas, and Numpy to clean Data as well as preform analysis.",
         image: "./images/projects/bananalysis.png",
         link: "https://calwilee.github.io/LoLEDA/",
     },
